2b9ceec875597942c4f0cac8608200010905c255
Adarsha Kumar <118099739+adarshakumar395@users.noreply.github.com>
2024-01-31 22:06:06 +0530

    Add files via upload

---
+++ ADVITIYA1.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+int N;
+	cin >> N;
+	switch (N){
+	case 16:
+	cout << "ADVITIYA";
+	break;
+	
+	case 17:
+	cout << "ADVITIYA";
+	break;
+	
+	case 18:
+	cout <<  "ADVITIYA";
+	break;
+	
+	default:
+	cout <<"WAITING FOR ADVITIYA";
+	
+	}	
+	
+	
+	
+	
+
+}
